0d59243dd6bf203f5d7d10411640c8986a9dd21d
simon <thevret.simon@gmail.com>
2017-11-04 16:52:45 +0100

    ajout de bcp.c (exercice 6) qui est a rendre lui aussi. comme il utilise bwrite il n'a pas encor été testé

---
+++ bio/bcp.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "bio.h"
+
+#define BUFFSZ 1024
+
+
+
+int main(int argc,char* argv[]){
+  if(argc != 3){
+    fprintf(stderr,"usage: %s sourceFile targetFile\n",argv[0]);
+    return 1;
+  }
+  BFILE* source;
+  if((source = bopen(argv[1],"r")) == NULL){
+    perror("bopen");
+    return 2;
+  }
+  BFILE* cible;
+  if((cible = bopen(argv[2],"w")) == NULL){
+    bclose(source);
+    perror("bopen");
+    return 2;
+  }
+  char buf[BUFFSZ];
+  ssize_t tailleLue = 1;
+  ssize_t tailleEcrie;
+  while(!beof(source) && tailleLue != 0){
+    tailleLue = bread(buf,BUFFSZ,source);
+    tailleEcrie = bwrite(buf,tailleLue,cible);
+    if(tailleEcrie != tailleLue){
+      perror("bwrite");
+      break;
+    }
+  }
+  bclose(source);
+  bclose(cible);
+  return 0;
+}
+
