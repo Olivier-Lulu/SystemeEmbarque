8f5cd58e218f39a240bdddbae750c143120cbbb7
LuLu <olivier.gracianne@gmail.com>
2017-11-05 18:31:02 +0100

    svg bcp avant pull

--- bio/bcp.c
+++ bio/bcp.c
@@ -3,19 +3,19 @@
 
 #define BUFFSZ 1024
 
-
-
 int main(int argc,char* argv[]){
   if(argc != 3){
     fprintf(stderr,"usage: %s sourceFile targetFile\n",argv[0]);
     return 1;
   }
   BFILE* source;
+    fprintf(stdout, "open1\n");
   if((source = bopen(argv[1],"r")) == NULL){
     perror("bopen");
     return 2;
   }
   BFILE* cible;
+    fprintf(stdout, "open2\n");
   if((cible = bopen(argv[2],"w")) == NULL){
     bclose(source);
     perror("bopen");
@@ -24,8 +24,11 @@ int main(int argc,char* argv[]){
   char buf[BUFFSZ];
   ssize_t tailleLue = 1;
   ssize_t tailleEcrie;
+  fprintf(stdout, "copie\n");
   while(!beof(source) && tailleLue != 0){
+    fprintf(stdout, "read\n");
     tailleLue = bread(buf,BUFFSZ,source);
+    fprintf(stdout, "write\n");
     tailleEcrie = bwrite(buf,tailleLue,cible);
     if(tailleEcrie != tailleLue){
       perror("bwrite");
